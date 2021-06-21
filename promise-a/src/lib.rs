d4fdd5f95c3e08947f607fd1a0f7e453a17bc179
wjsxqs <xsmlcp@163.com>
2021-06-21 11:37:11 +0800

    Add some test

--- promise-a/src/lib.rs
+++ promise-a/src/lib.rs
@@ -1,5 +1,5 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, log, metadata, near_bindgen, setup_alloc};
+use near_sdk::{env, ext_contract, log, metadata, near_bindgen, setup_alloc, AccountId};
 use near_sdk::serde_json;
 use near_sdk::json_types::Base64VecU8;
 
@@ -7,18 +7,80 @@ use std::collections::HashMap;
 
 setup_alloc!();
 
+pub const SINGLE_CALL_GAS: u64 = 20_000_000_000_000; // 2 x 10^14
+
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct Contract {}
 
 #[near_bindgen]
 impl Contract {
-    pub fn get_message(&self) -> String {
-        log!("I am {}. Called by {}", env::current_account_id(), env::predecessor_account_id());
+    pub fn set_message(&self, account_a: AccountId) -> String {
+        log!(
+            "Method: set_message. I am {}. Called by {}.",
+            env::current_account_id(),
+            env::predecessor_account_id(),
+        );
 
+        log!("Param account_a {}", account_a);
+
+        let prepaid_gas = env::prepaid_gas();
+        log!("prepaid_gas {}", prepaid_gas);
+
+        ext_promise_a::get_message(
+            &account_a,
+            1,
+            SINGLE_CALL_GAS
+        )
+        .then(ext_self::on_data(
+            &env::current_account_id(),
+            0,
+            SINGLE_CALL_GAS
+        ));
+
+        log!("after ext call");
+
+        "World".to_string()
+    }
+
+    pub fn get_message(&self, message: String) -> String {
+        log!("Method: get_message. I am {}. Called by {}", env::current_account_id(), env::predecessor_account_id());
+
+        // "Hello".to_string()
+        message
+    }
+
+    pub fn on_data(
+        &mut self,
+        #[callback] value_of_a: String,
+    ) -> String {
+        log!(
+            "Method: on_data. I am {}. Called by {}. value_of_a {}",
+            env::current_account_id(),
+            env::predecessor_account_id(),
+            value_of_a,
+        );
+
+        format!("on_data {}", value_of_a)
+    }
+
+    fn get_random(&self) -> String {
         let random = env::random_seed();
         log!("random {:?}", random);
-        // "Hello".to_string()
         serde_json::to_string(&Base64VecU8(random)).unwrap()
     }
+}
+
+#[ext_contract(ext_promise_a)]
+pub trait ExtPromiseA {
+    fn set_message(&self, account_a: AccountId) -> String;
+    fn get_message(&self) -> String;
+}
+
+#[ext_contract(ext_self)]
+pub trait ExtContract {
+    fn on_data(
+        &mut self,
+        #[callback] value_of_a: String,
+    ) -> String;
 }
--- promise-b/src/lib.rs
+++ promise-b/src/lib.rs
@@ -1,11 +1,11 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, ext_contract, log, metadata, near_bindgen, setup_alloc, AccountId};
-
-use std::collections::HashMap;
+use near_sdk::{env, ext_contract, log, metadata, near_bindgen, setup_alloc, AccountId, Promise};
 
 setup_alloc!();
 
 pub const SINGLE_CALL_GAS: u64 = 20_000_000_000_000; // 2 x 10^14
+
+pub const FINISH_CALL_A_GAS: u64 = 30_000_000_000_000 + SINGLE_CALL_GAS;
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
@@ -14,56 +14,122 @@ pub struct Contract {}
 #[near_bindgen]
 impl Contract {
     pub fn call_a(&mut self, account_a: String, account_a2: String) {
-        let sender = env::predecessor_account_id();
-        log!("sender {}", sender);
+        log!(
+            "Method: call_a. I am {}. Called by {}. call_a",
+            env::current_account_id(),
+            env::predecessor_account_id(),
+        );
+
+        log!("Param account_a {}, account_a2 {}", account_a, account_a2);
+
+        let prepaid_gas = env::prepaid_gas();
+        log!("prepaid_gas {}", prepaid_gas);
+
+        // ext_promise_a::get_message(
+        //     &account_a,
+        //     1,
+        //     SINGLE_CALL_GAS
+        // )
+        // .then(ext_self::on_data(
+        //     account_a,
+        //     &env::current_account_id(),
+        //     0,
+        //     SINGLE_CALL_GAS
+        // ));
 
         ext_promise_a::get_message(
+            "Hello".to_string(),
             &account_a,
             1,
             SINGLE_CALL_GAS
         )
-        .and(ext_promise_a::get_message(
-            &account_a2,
+        .then(ext_self::finish_call_a(
+            account_a2,
+            &env::current_account_id(),
             1,
-            SINGLE_CALL_GAS
+            FINISH_CALL_A_GAS
         ))
         .then(ext_self::on_data(
+            account_a,
             &env::current_account_id(),
             0,
             SINGLE_CALL_GAS
         ));
 
-        log!("after call");
+        // ext_promise_a::set_message(
+        //     account_a2,
+        //     &account_a,
+        //     1,
+        //     prepaid_gas
+        // )
+        // .then(ext_self::on_data(
+        //     account_a,
+        //     &env::current_account_id(),
+        //     0,
+        //     prepaid_gas
+        // ));
+
+        log!("after ext call");
         env::value_return(b"Test");
+    }
+
+    pub fn finish_call_a(
+        &self,
+        #[callback]
+        message: String,
+        account_a: AccountId,
+    ) -> Promise {
+        log!(
+            "Method: finish_call_a. I am {}. Called by {}. message {}, account_a {}",
+            env::current_account_id(),
+            env::predecessor_account_id(),
+            message,
+            account_a
+        );
+
+        ext_promise_a::get_message(
+            "World".to_string(),
+            &account_a,
+            1,
+            SINGLE_CALL_GAS
+        )
     }
 
     pub fn on_data(
         &mut self,
+        account_a: AccountId,
         #[callback] value_of_a: String,
-        #[callback] value_of_a2: String
     ) -> String {
         log!(
-            "I am {}. Called by {}. on_data: a {}, a2 {}",
+            "Method: on_data. I am {}. Called by {}. value_of_a {}",
             env::current_account_id(),
             env::predecessor_account_id(),
             value_of_a,
-            value_of_a2,
         );
 
-        format!("on_data {}-{}", value_of_a, value_of_a2)
+        format!("on_data {}-{}", account_a, value_of_a)
     }
 }
 
 #[ext_contract(ext_promise_a)]
 pub trait ExtPromiseA {
-    fn get_message(&self) -> String;
+    fn set_message(&self, account_a: AccountId) -> String;
+    fn get_message(&self, message: String) -> String;
 }
 
 #[ext_contract(ext_self)]
 pub trait ExtContract {
     fn on_data(
         &mut self,
+        account_a: AccountId,
         #[callback] value_of_a: String,
     ) -> String;
+
+    fn finish_call_a(
+        &self,
+        #[callback]
+        message: String,
+        account_a: AccountId,
+    ) -> Promise;
 }
 
